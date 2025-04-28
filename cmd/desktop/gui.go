3f5c721e216db016fed0a79df0e3a079b626c8d2
e-felix <42306143+e-felix@users.noreply.github.com>
2025-04-28 22:33:54 +0200

    Refactoring UI with custom layout

--- cmd/desktop/gui.go
+++ cmd/desktop/gui.go
@@ -3,33 +3,50 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
 const (
 	TOP_BAR_TITLE              = "Sebas - Your Personal Butler"
 	PROJECT_SELECT_PLACEHOLDER = "Select a project"
+
+	// Menu
+	FILE_MENU      = "File"
+	FILE_MENU_OPEN = "Open File..."
+
+	// Project
+	PROJECT_ADD_BUTTON    = "Add new"
+	PROJECT_EDIT_BUTTON   = "Edit"
+	PROJECT_DELETE_BUTTON = "Delete"
 )
 
 type gui struct {
-	win fyne.Window
+	win  fyne.Window
+	file binding.String
 }
 
-func makeTopBar() fyne.CanvasObject {
+func (g *gui) makeTopBar() fyne.CanvasObject {
 	title := widget.NewLabel(TOP_BAR_TITLE)
 
 	projectSelect := widget.NewSelect([]string{"Project 1", "Project 2", "Project 3"}, func(selected_value string) {})
 	projectSelect.PlaceHolder = PROJECT_SELECT_PLACEHOLDER
 
-	return container.NewHBox(title, projectSelect)
+	projectAdd := widget.NewButtonWithIcon(PROJECT_ADD_BUTTON, theme.Icon(theme.IconNameContentAdd), func() {})
+	projectEdit := widget.NewButtonWithIcon(PROJECT_EDIT_BUTTON, theme.Icon(theme.IconNameDocumentCreate), func() {})
+	projectDelete := widget.NewButtonWithIcon(PROJECT_DELETE_BUTTON, theme.Icon(theme.IconNameDelete), func() {})
+
+	return container.NewGridWithColumns(2, title, container.NewHBox(projectAdd, projectEdit, projectDelete, projectSelect))
 }
 
 func (g *gui) makeUi() fyne.CanvasObject {
-	top := makeTopBar()
+	top := g.makeTopBar()
 	footer := widget.NewLabel("Footer")
 	leftMenu := widget.NewLabel("Left Menu")
-	content := widget.NewLabel("Content")
+
+	content := widget.NewLabelWithData(g.file)
 
 	topDivider := widget.NewSeparator()
 	leftDivider := widget.NewSeparator()
@@ -44,6 +61,43 @@ func (g *gui) makeUi() fyne.CanvasObject {
 	return container.New(newSebasLayout(top, footer, leftMenu, content, dividers), objs...)
 }
 
-func (g *gui) openDatabase() {
-	dialog.ShowFileOpen(func(reader fyne.URIReadCloser, err error) {}, g.win)
+func (g *gui) makeMenu() *fyne.MainMenu {
+	return fyne.NewMainMenu(
+		fyne.NewMenu(
+			FILE_MENU,
+			fyne.NewMenuItem(
+				FILE_MENU_OPEN,
+				func() {
+					g.openFileDialog()
+				},
+			),
+		),
+	)
+}
+
+func (g *gui) openFileDialog() {
+	dialog.ShowFileOpen(func(reader fyne.URIReadCloser, err error) {
+		if err != nil {
+			dialog.ShowError(err, g.win)
+			return
+		}
+
+		if reader == nil {
+			return
+		}
+
+		g.openFile(reader)
+	}, g.win)
+
+}
+
+func (g *gui) openFile(reader fyne.URIReadCloser) {
+	filename := reader.URI().Name()
+
+	err := g.file.Set(filename)
+
+	if err != nil {
+		dialog.ShowError(err, g.win)
+		return
+	}
 }
