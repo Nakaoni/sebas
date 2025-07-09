ecd2c3afbf51aaf5267fbd82c37863da07a59bcc
Nakaoni <16108885+Nakaoni@users.noreply.github.com>
2025-07-09 23:23:40 +0200

    Add run method for command

--- internal/command/command.go
+++ internal/command/command.go
@@ -1,5 +1,12 @@
 package command
 
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os/exec"
+)
+
 type Command struct {
 	Cmd  string
 	Args []string
@@ -18,4 +25,35 @@ func (command *Command) UpdateCmd(cmd string) {
 
 func (command *Command) UpdateArgs(args []string) {
 	command.Args = args
+}
+
+func (command *Command) Run(ctx context.Context, output chan string) {
+	cmd := exec.CommandContext(ctx, command.Cmd, command.Args...)
+	defer close(output)
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		output <- fmt.Sprint(err)
+		return
+	}
+
+	if err := cmd.Start(); err != nil {
+		output <- fmt.Sprint(err)
+		return
+	}
+
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		output <- scanner.Text()
+	}
+
+	if err := scanner.Err(); err != nil {
+		output <- fmt.Sprint(err)
+		return
+	}
+
+	if err := cmd.Wait(); err != nil {
+		output <- fmt.Sprint(err)
+		return
+	}
 }
--- internal/command/command_test.go
+++ internal/command/command_test.go
@@ -1,6 +1,7 @@
 package command
 
 import (
+	"context"
 	"testing"
 
 	Assert "github.com/e-felix/sebas/internal/util/assert"
@@ -44,4 +45,22 @@ func TestCommandUpdateArgs(t *testing.T) {
 	command.UpdateArgs(newArgs)
 
 	Assert.DeepEqual(command, expected)
+}
+
+func TestCommandRun(t *testing.T) {
+	expected := "Hello"
+
+	cmd := "echo"
+	args := make([]string, 0)
+	args = append(args, "Hello")
+
+	command := &Command{Cmd: cmd, Args: args}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	output := make(chan string)
+
+	go command.Run(ctx, output)
+	value := <-output
+
+	Assert.DeepEqual(value, expected)
 }
