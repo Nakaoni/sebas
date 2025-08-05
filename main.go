3a2d8c2f8e61ae39e590f386fed0cd0cae154cdc
Nakaoni <16108885+Nakaoni@users.noreply.github.com>
2025-08-05 22:24:24 +0200

    implementing migration system

--- main.go
+++ main.go
@@ -1,59 +1,117 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	"os"
+	"os/exec"
+	"path"
+	"plugin"
+	"strings"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/nakaoni/sebas/internal/util"
-	repo "github.com/nakaoni/sebas/repository"
 )
 
-func main() {
+func initDB(db *sql.DB) {
+	cwd, _ := os.Getwd()
+	basePath := path.Join(cwd, "migrations")
+	dirEntry, _ := os.ReadDir(basePath)
 
+	for _, entry := range dirEntry {
+		if entry.IsDir() {
+			continue
+		}
+		name := entry.Name()
+		versionPath := path.Join(basePath, name)
+
+		compiledVersionPath := path.Join(os.TempDir(), strings.Replace(name, ".go", ".so", 1))
+		cmd := exec.Command("go", "build", "-buildmode=plugin", "-o", compiledVersionPath, versionPath)
+		if err := cmd.Run(); err != nil {
+			panic(err)
+		}
+
+		p, err := plugin.Open(compiledVersionPath)
+		if err != nil {
+			panic(err)
+		}
+
+		f, err := p.Lookup("Up")
+		if err != nil {
+			panic(err)
+		}
+
+		tx, err := db.Begin()
+		if err != nil {
+			panic(err)
+		}
+		defer tx.Rollback()
+
+		up := f.(func() string)()
+		if _, err = tx.Exec(up); err != nil {
+			panic(err)
+		}
+
+		_, err = db.Exec(`
+		INSERT INTO migrations (version, created_at, executed_at)
+		VALUES ($1, NOW(), NOW())
+		`, name)
+		if err != nil {
+			panic(err)
+		}
+
+		if err := tx.Commit(); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func main() {
 	db, err := util.GetDatabase()
 	if err != nil {
 		panic(err)
 	}
 
-	result, err := db.Exec(`
-	DROP TABLE commands;
-	`)
-	if err != nil {
-		panic(err)
-	}
-	i, _ := result.RowsAffected()
-	fmt.Println("drop table: ", i)
+	initDB(db)
 
-	result, err = db.Exec(`
-	CREATE TABLE commands (
-		id INT PRIMARY KEY,
-		cmd VARCHAR(500),
-		args VARCHAR(500)
-	)
-	`)
-	if err != nil {
-		panic(err)
-	}
+	// result, err := db.Exec(`
+	// DROP TABLE IF EXISTS commands;
+	// `)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// i, _ := result.RowsAffected()
+	// fmt.Println("drop table: ", i)
 
-	i, _ = result.RowsAffected()
-	fmt.Println("create table: ", i)
+	// result, err = db.Exec(`
+	// CREATE TABLE commands (
+	// 	id INT PRIMARY KEY,
+	// 	cmd VARCHAR(500),
+	// 	args VARCHAR(500)
+	// )
+	// `)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	result, err = db.Exec(`
-	INSERT INTO commands (cmd, args)
-	VALUES
-	("echo", "hello world"),
-	("ls", "-la")
-	`)
-	if err != nil {
-		panic(err)
-	}
+	// i, _ = result.RowsAffected()
+	// fmt.Println("create table: ", i)
 
-	i, _ = result.RowsAffected()
-	fmt.Println("rows: ", i)
+	// result, err = db.Exec(`
+	// INSERT INTO commands (cmd, args)
+	// VALUES
+	// ("echo", "hello world"),
+	// ("ls", "-la")
+	// `)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	cmds := repo.CommandAll()
+	// i, _ = result.RowsAffected()
+	// fmt.Println("rows: ", i)
 
-	for _, c := range cmds {
-		fmt.Println(c)
-	}
+	// cmds := repo.CommandAll()
+
+	// for _, c := range cmds {
+	// 	fmt.Println(c)
+	// }
 }
